2163e0262e1e25840058a1f70279a9f7e551f953
taewoon123 <120540537+taewoon123@users.noreply.github.com>
2023-03-27 10:39:03 +0900

    백: 입양홍보&후기 컨트롤러 dao vo 생성

---
+++ workspace/semiWebPrj01/src/com/kh/app/adopt/service/AdoptReviewService.java
@@ -0,0 +1,73 @@
+package com.kh.app.adopt.service;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.kh.app.adopt.dao.AdoptReviewDao;
+import com.kh.app.adopt.vo.AdoptVo;
+import com.kh.app.util.JDBCTemplate;
+
+public class AdoptReviewService {
+
+	public List<AdoptVo> reviewList() throws Exception {
+
+		//비즈니스 로직
+		
+		//conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+		//SQL
+		AdoptReviewDao reviewdao = new AdoptReviewDao();
+		List<AdoptVo> adrList = reviewdao.reviewList(conn);
+		
+		//close
+		JDBCTemplate.close(conn);
+		
+		return adrList;
+		
+	}
+	
+	//입양후기상세조회
+	public AdoptVo selectOne(String adpNo) throws Exception {
+		
+		//비즈니스 로직
+		
+		//conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+		//DAO 호출
+		AdoptReviewDao reviewdao = new AdoptReviewDao();
+		AdoptVo adoptVo = reviewdao.selectOne(conn , adpNo);
+		
+		//tx , close
+		JDBCTemplate.close(conn);
+		
+		return adoptVo;
+		
+	}
+	
+	//입양후기게시글작성
+	public int ReviewWrite(AdoptVo vo) throws Exception {
+		
+		//비즈니스 로직
+		
+		//conn
+		Connection conn = JDBCTemplate.getConnection();
+		
+		//SQL (DAO)
+		AdoptReviewDao dao = new AdoptReviewDao();
+		int result = dao.ReviewWrite(conn , vo);
+		
+		//tx , close
+		if(result == 1) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		
+		JDBCTemplate.close(conn);
+		
+		return result;
+	}
+	
+}//class
