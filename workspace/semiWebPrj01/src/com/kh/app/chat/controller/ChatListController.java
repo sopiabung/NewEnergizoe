6be24d85262b167f4a456c3d7587f011dd4b43ff
tpwhd <120540578+tpwhd@users.noreply.github.com>
2023-03-27 17:20:05 +0900

    백 : 컨트롤러 어더텐션 수정

--- workspace/semiWebPrj01/src/com/kh/app/chat/controller/ChatListController.java
+++ workspace/semiWebPrj01/src/com/kh/app/chat/controller/ChatListController.java
@@ -9,52 +9,54 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.app.chat.service.ChatService;
+import com.kh.app.chat.vo.ChatVo;
+import com.kh.app.util.page.PageVo;
+
 //변동가능!!
-@WebServlet("/board/chat")
+@WebServlet("/chat/list")
 public class ChatListController extends HttpServlet {
 	
+	private ChatService bs = new ChatService();
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-try {
-			
+
+		try {
+
 			//데이터 꺼내기(데이터 준비)
 			int currentPage = Integer.parseInt(req.getParameter("page"));	
-			int listCount = bs.selectCount();
+//			int listCount = bs.selectChatList();
 			int pageLimit = 5;
 			int	boardLimit = 11;
-			
-			
-			
-			
-			
+
 			//데이터 뭉치기
-			PageVo pageVo = new PageVo(listCount, currentPage, pageLimit, boardLimit);
-			
+//			PageVo pageVo = new PageVo(listCount, currentPage, pageLimit, boardLimit);
+
 			//서비스 호출
-			List<BoardVo> boardList = bs.selectList(pageVo);
-				
-			
-			
+//			List<ChatVo> boardList = bs.selectChatList(pageVo);
+
+
+
 			//화면
-			req.setAttribute("boardList", boardList);
-			req.setAttribute("pageVo", pageVo);
+//			req.setAttribute("boardList", boardList);
+//			req.setAttribute("pageVo", pageVo);
 			req.getRequestDispatcher("/WEB-INF/views/board/list.jsp").forward(req, resp);
-			
+
 		} catch (Exception e) {
 			System.out.println("[ERROR] 게시글 조회 중 예외 발생 ...");
 			e.printStackTrace();
 			req.getRequestDispatcher("/WEB-INF/views/common/error.jsp").forward(req, resp);
 		}
-		
-		
+
+
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
 	}
-	
-	
+
+
 
 }
