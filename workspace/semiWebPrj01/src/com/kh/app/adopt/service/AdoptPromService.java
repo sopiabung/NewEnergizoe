124857330abf809898a253f8db7a303bf5178bdd
taewoon123 <120540537+taewoon123@users.noreply.github.com>
2023-03-30 13:21:46 +0900

    입양홍보쪽 전체수정

--- workspace/semiWebPrj01/src/com/kh/app/adopt/service/AdoptPromService.java
+++ workspace/semiWebPrj01/src/com/kh/app/adopt/service/AdoptPromService.java
@@ -1,66 +1,29 @@
 package com.kh.app.adopt.service;
 
 import java.sql.Connection;
-import java.util.List;
 
 import com.kh.app.adopt.dao.AdoptPromDao;
-import com.kh.app.adopt.vo.AdoptVo;
-import com.kh.app.adopt.vo.AniadoptVo;
+import com.kh.app.animal.vo.AnimalVo;
+import com.kh.app.board.vo.AdoptPhotoVo;
 import com.kh.app.util.JDBCTemplate;
-import com.kh.app.util.page.PageVo;
 
 public class AdoptPromService {
 
-	public List<AniadoptVo> promList(PageVo pagevo) throws Exception {
-		//비즈니스 로직
+	//입양홍보글 작성
+	public int write(AnimalVo vo, AdoptPhotoVo atVo) throws Exception {
+		//비지니스 로직
 		
 		//conn
 		Connection conn = JDBCTemplate.getConnection();
 		
-		//SQL
-		AdoptPromDao promdao = new AdoptPromDao();
-		List<AniadoptVo> adpList = promdao.promList(conn , pagevo);
-		
-		//close
-		JDBCTemplate.close(conn);
-		
-		return adpList;
-	}
-	
-	
-	//입양홍보상세조회
-	public AdoptVo selectOne(String adpNo) throws Exception {
-		
-		//비즈니스 로직
-		
-		//conn
-		Connection conn = JDBCTemplate.getConnection();
-		
-		//DAO 호출
-		AdoptPromDao promdao = new AdoptPromDao();
-		AdoptVo adoptVo = promdao.selectOne(conn , adpNo);
-		
-		//tx , close
-		JDBCTemplate.close(conn);
-		
-		return adoptVo;
-		
-	}
-
-	//입양홍보게시글작성
-	public int PromWrite(AdoptVo vo) throws Exception {
-		
-		//비즈니스 로직
-		
-		//conn
-		Connection conn = JDBCTemplate.getConnection();
-		
-		//SQL (DAO)
 		AdoptPromDao dao = new AdoptPromDao();
-		int result = dao.PromWrite(conn , vo);
+		//SQL (DAO) == 게시글 인서트
+		int result = dao.write(conn , vo);
+		//SQL (DAO) == 첨부파일 인서트
+		int atResult = dao.insertAttachment(conn, atVo);
 		
 		//tx , close
-		if(result == 1) {
+		if(result * atResult == 1) {
 			JDBCTemplate.commit(conn);
 		}else {
 			JDBCTemplate.rollback(conn);
@@ -68,29 +31,68 @@ public class AdoptPromService {
 		
 		JDBCTemplate.close(conn);
 		
-		return result;
-		
+		return result * atResult;
 	}
 
-	//게시글 전체 갯수 조회 (삭제되지않은)
-	public int selectCount() throws Exception {
-		
-		//비지니스 로직
-		
-		//conn
-		Connection conn = JDBCTemplate.getConnection();
-		
-		//SQL (DAO)
-		AdoptPromDao dao = new AdoptPromDao();
-		int result = dao.selectCount(conn);
-		
-		//close
-		JDBCTemplate.close(conn);
-		
-		return result;
-	}
-
-
+	
+	
+//	public List<AniadoptVo> promList(PageVo pagevo) throws Exception {
+////		//비즈니스 로직
+//		
+////		//conn
+//		Connection conn = JDBCTemplate.getConnection();
+//	
+////		//SQL
+//		AdoptPromDao promdao = new AdoptPromDao();
+//		//List<AniadoptVo> adpList = promdao.promList(conn , pagevo);
+//		
+////		//close
+//		JDBCTemplate.close(conn);
+//		
+//		return adpList;
+//	}
+//	
+//	
+////	//입양홍보상세조회
+//	public AdoptVo selectOne(String adpNo) throws Exception {
+//		
+////		//비즈니스 로직
+//		
+////		//conn
+//		Connection conn = JDBCTemplate.getConnection();
+//		
+////		//DAO 호출
+//		AdoptPromDao promdao = new AdoptPromDao();
+//		AdoptVo adoptVo = promdao.selectOne(conn , adpNo);
+//		
+////		//tx , close
+//		JDBCTemplate.close(conn);
+//		
+//		return adoptVo;
+//		
+//	}
+//
+//
+//	//게시글 전체 갯수 조회 (삭제되지않은)
+//	public int selectCount() throws Exception {/		
+//		//비지니스 로직
+//		
+//		//conn
+//		Connection conn = JDBCTemplate.getConnection();
+//		
+//		//SQL (DAO)
+//		AdoptPromDao dao = new AdoptPromDao();
+//		int result = dao.selectCount(conn);
+//		
+//		//close
+//		JDBCTemplate.close(conn);
+//		
+//		return result;
+//	}
+//
+//
+//
+//
 
 
 	
